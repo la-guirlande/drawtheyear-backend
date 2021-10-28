d9f2d0a6c1e0539ac793a850ceda4be23afae6ab
Lelberto <jsurieux@gmail.com>
2021-10-28 12:46:19 +0200

    Add emotion model

--- src/services/database-service.ts
+++ src/services/database-service.ts
@@ -1,4 +1,5 @@
 import { Model, Mongoose } from 'mongoose';
+import createEmotionModel, { EmotionInstance } from '../models/emotion-model';
 import createUserModel, { UserInstance } from '../models/user-model';
 import Service from './service';
 import ServiceContainer from './service-container';
@@ -11,6 +12,7 @@ import ServiceContainer from './service-container';
 export default class DatabaseService extends Service {
 
   public readonly users: Model<UserInstance>;
+  public readonly emotions: Model<EmotionInstance>;
   private readonly mongoose: Mongoose;
 
   /**
@@ -22,6 +24,7 @@ export default class DatabaseService extends Service {
     super(container);
     this.mongoose = this.createMongoose();
     this.users = createUserModel(container, this.mongoose);
+    this.emotions = createEmotionModel(container, this.mongoose);
   }
 
   /**
