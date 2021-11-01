2635a6f9601f19802b1656e2eb31ab6ec3bf5a47
Lelberto <jsurieux@gmail.com>
2021-11-01 21:19:49 +0100

    Move (again) user requests from user model request to user and self controllers

--- src/controllers/self-controller.ts
+++ src/controllers/self-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import _ from 'lodash';
+import { Error as MongooseError } from 'mongoose';
 import { UserDocument } from '../models/user-model';
 import ServiceContainer from '../services/service-container';
 import Controller from './controller';
@@ -13,10 +15,10 @@ export default class SelfController extends Controller {
   public constructor(container: ServiceContainer) {
     super(container, '/me');
     this.registerEndpoint({ method: 'GET', uri: '/', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, this.infoHandler] });
-    this.registerEndpoint({ method: 'GET', uri: '/emotions', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, container.db.userMR.listEmotionsHandler] });
-    this.registerEndpoint({ method: 'POST', uri: '/emotions', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, container.db.userMR.createEmotionHandler] });
-    this.registerEndpoint({ method: 'PATCH', uri: '/emotions/:emotionId', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, container.db.userMR.updateEmotionHandler] });
-    this.registerEndpoint({ method: 'DELETE', uri: '/emotions/:emotionId', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, container.db.userMR.deleteEmotionHandler] });
+    this.registerEndpoint({ method: 'GET', uri: '/emotions', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, this.listEmotionsHandler] });
+    this.registerEndpoint({ method: 'POST', uri: '/emotions', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, this.createEmotionHandler] });
+    this.registerEndpoint({ method: 'PATCH', uri: '/emotions/:emotionId', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, this.updateEmotionHandler] });
+    this.registerEndpoint({ method: 'DELETE', uri: '/emotions/:emotionId', handlers: [container.auth.authenticateHandler, container.auth.isAuthenticatedHandler, this.deleteEmotionHandler] });
   }
 
   /**
@@ -30,20 +32,106 @@ export default class SelfController extends Controller {
    */
    public async infoHandler(req: Request, res: Response): Promise<Response> {
     try {
+      return res.status(200).json({ user: res.locals.authUser });
+    } catch (err) {
+      this.logger.error(err);
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
+  }
+
+  /**
+   * Lists all user's emotions.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @async
+   */
+  public async listEmotionsHandler(req: Request, res: Response): Promise<Response> {
+    try {
+      return res.status(200).send({ emotions: (res.locals.authUser as UserDocument).emotions.filter(emotion => !emotion.deleted) });
+    } catch (err) {
+      this.logger.error(err);
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
+  }
+
+  /**
+   * Creates a new emotion.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @async
+   */
+  public async createEmotionHandler(req: Request, res: Response): Promise<Response> {
+    try {
       const authUser: UserDocument = res.locals.authUser;
-      if (authUser == null) {
-        return res.status(404).json(this.container.errors.formatErrors({
+      authUser.emotions.push(req.body);
+      await authUser.save();
+      return res.status(201).send({ id: _.last(authUser.emotions).id });
+    } catch (err) {
+      this.logger.error(err);
+      if (err instanceof MongooseError.ValidationError) {
+        return res.status(400).send(this.container.errors.formatErrors(...this.container.errors.translateMongooseValidationError(err)));
+      }
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
+  }
+
+  /**
+   * Updates an emotion.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @async
+   */
+  public async updateEmotionHandler(req: Request, res: Response): Promise<Response> {
+    const { name, color } = req.body;
+    try {
+      const authUser: UserDocument = res.locals.authUser;
+      const emotion = authUser.emotions.find(emotion => !emotion.deleted && emotion.id === req.params.emotionId);
+      if (emotion == null) {
+        return res.status(404).send(this.container.errors.formatErrors({
           error: 'not_found',
-          error_description: 'User not found'
+          error_description: 'Emotion not found'
         }));
       }
-      if (!authUser.hasPermission('own.read')) {
-        return res.status(403).json(this.container.errors.formatErrors({
-          error: 'forbidden',
-          error_description: 'Permission denied'
+      if (name != null) {
+        emotion.name = name;
+      }
+      if (color != null) {
+        emotion.color = color;
+      }
+      await authUser.save();
+      return res.status(200).send({ id: emotion.id });
+    } catch (err) {
+      this.logger.error(err);
+      if (err instanceof MongooseError.ValidationError) {
+        return res.status(400).send(this.container.errors.formatErrors(...this.container.errors.translateMongooseValidationError(err)));
+      }
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
+  }
+
+  /**
+   * Deletes an emotion.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @async
+   */
+  public async deleteEmotionHandler(req: Request, res: Response): Promise<Response> {
+    try {
+      const authUser: UserDocument = res.locals.authUser;
+      const emotion = authUser.emotions.find(emotion => !emotion.deleted && emotion.id === req.params.emotionId);
+      if (emotion == null) {
+        return res.status(404).send(this.container.errors.formatErrors({
+          error: 'not_found',
+          error_description: 'Emotion not found'
         }));
       }
-      return res.status(200).json({ user: authUser });
+      emotion.deleted = true;
+      await authUser.save();
+      return res.status(204).send();
     } catch (err) {
       this.logger.error(err);
       return res.status(500).send(this.container.errors.formatServerError());
--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import _ from 'lodash';
+import { Error as MongooseError } from 'mongoose';
 import ServiceContainer from '../services/service-container';
 import Controller from './controller';
 
@@ -19,10 +21,10 @@ export default class UserController extends Controller {
     this.registerEndpoint({ method: 'GET', uri: '/', handlers: this.listHandler });
     this.registerEndpoint({ method: 'GET', uri: '/:id', handlers: this.getHandler });
     this.registerEndpoint({ method: 'DELETE', uri: '/:id', handlers: this.deleteHandler });
-    this.registerEndpoint({ method: 'GET', uri: '/:id/emotions', handlers: container.db.userMR.listEmotionsHandler });
-    this.registerEndpoint({ method: 'POST', uri: '/:id/emotions', handlers: container.db.userMR.createEmotionHandler });
-    this.registerEndpoint({ method: 'PATCH', uri: '/:id/emotions/:emotionId', handlers: container.db.userMR.updateEmotionHandler });
-    this.registerEndpoint({ method: 'DELETE', uri: '/:id/emotions/:emotionId', handlers: container.db.userMR.deleteEmotionHandler });
+    this.registerEndpoint({ method: 'GET', uri: '/:id/emotions', handlers: this.listEmotionsHandler });
+    this.registerEndpoint({ method: 'POST', uri: '/:id/emotions', handlers: this.createEmotionHandler });
+    this.registerEndpoint({ method: 'PATCH', uri: '/:id/emotions/:emotionId', handlers: this.updateEmotionHandler });
+    this.registerEndpoint({ method: 'DELETE', uri: '/:id/emotions/:emotionId', handlers: this.deleteEmotionHandler });
   }
 
   /**
@@ -87,6 +89,130 @@ export default class UserController extends Controller {
         }));
       }
       user.deleted = true;
+      await user.save();
+      return res.status(204).send();
+    } catch (err) {
+      this.logger.error(err);
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
+  }
+
+  /**
+   * Lists all user's emotions.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @async
+   */
+  public async listEmotionsHandler(req: Request, res: Response): Promise<Response> {
+    try {
+      const user = await this.db.users.findById(req.params.id).select('emotions');
+      if (user == null) {
+        return res.status(404).send(this.container.errors.formatErrors({
+          error: 'not_found',
+          error_description: 'User not found'
+        }));
+      }
+      return res.status(200).send({ emotions: user.emotions.filter(emotion => !emotion.deleted) });
+    } catch (err) {
+      this.logger.error(err);
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
+  }
+
+  /**
+   * Creates a new emotion.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @async
+   */
+   public async createEmotionHandler(req: Request, res: Response): Promise<Response> {
+    try {
+      const user = await this.db.users.findById(req.params.id).select('emotions');
+      if (user == null) {
+        return res.status(404).send(this.container.errors.formatErrors({
+          error: 'not_found',
+          error_description: 'User not found'
+        }));
+      }
+      user.emotions.push(req.body);
+      await user.save();
+      return res.status(201).send({ id: _.last(user.emotions).id });
+    } catch (err) {
+      this.logger.error(err);
+      if (err instanceof MongooseError.ValidationError) {
+        return res.status(400).send(this.container.errors.formatErrors(...this.container.errors.translateMongooseValidationError(err)));
+      }
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
+  }
+
+  /**
+   * Updates an emotion.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @async
+   */
+   public async updateEmotionHandler(req: Request, res: Response): Promise<Response> {
+    const { name, color } = req.body;
+    try {
+      const user = await this.db.users.findById(req.params.id).select('emotions');
+      if (user == null) {
+        return res.status(404).send(this.container.errors.formatErrors({
+          error: 'not_found',
+          error_description: 'User not found'
+        }));
+      }
+      const emotion = user.emotions.find(emotion => !emotion.deleted && emotion.id === req.params.emotionId);
+      if (emotion == null) {
+        return res.status(404).send(this.container.errors.formatErrors({
+          error: 'not_found',
+          error_description: 'Emotion not found'
+        }));
+      }
+      if (name != null) {
+        emotion.name = name;
+      }
+      if (color != null) {
+        emotion.color = color;
+      }
+      await user.save();
+      return res.status(200).send({ id: req.params.emotionId });
+    } catch (err) {
+      this.logger.error(err);
+      if (err instanceof MongooseError.ValidationError) {
+        return res.status(400).send(this.container.errors.formatErrors(...this.container.errors.translateMongooseValidationError(err)));
+      }
+      return res.status(500).send(this.container.errors.formatServerError());
+    }
+  }
+
+  /**
+   * Deletes an emotion.
+   * 
+   * @param req Express request
+   * @param res Express response
+   * @async
+   */
+   public async deleteEmotionHandler(req: Request, res: Response): Promise<Response> {
+    try {
+      const user = await this.db.users.findById(req.params.id).where('deleted').equals(false);
+      if (user == null) {
+        return res.status(404).send(this.container.errors.formatErrors({
+          error: 'not_found',
+          error_description: 'User not found'
+        }));
+      }
+      const emotion = user.emotions.find(emotion => !emotion.deleted && emotion.id === req.params.emotionId);
+      if (emotion == null) {
+        return res.status(404).send(this.container.errors.formatErrors({
+          error: 'not_found',
+          error_description: 'Emotion not found'
+        }));
+      }
+      emotion.deleted = true;
       await user.save();
       return res.status(204).send();
     } catch (err) {
